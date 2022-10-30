312585fe4dc8173c0d834bf0b522440a869ee252
Shakhya <poorni.dasanayake1@students.vu.edu.au>
2022-10-30 11:52:07 +1100

    Assignment 1 part c

---
+++ compute.py
@@ -0,0 +1,19 @@
+class Figure:                                            #creating a new class.
+    def __init__(self, count, name, area):               #used __int__ function.
+        self.count = count
+        self.name = name                                 #self parameters to access count, name and area.
+        self.area = area
+        print(count, '-', name, 'with area size', area)  #printing count , name and area.
+
+data = [
+    {"type": "Square", "area": 150.5},
+    {"type": "Rectangle", "area": 80},
+    {"type": "Rectangle", "area": 660},                  #using given list and assigning to variables.
+    {"type": "Circle", "area": 68.2},
+    {"type": "Triangle", "area": 20}
+]
+
+count=1                                                 #intialising count to 1.
+for i in data:
+    Figure(str(count), i["type"], i["area"])
+    count += 1                                          #for loop to iterate data.
